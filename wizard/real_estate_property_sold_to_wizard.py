bc18bd6fae3080624ce28265d19ce750c8afffeb
arielrecto04 <ariel.recto@innovatotec.com>
2026-02-12 17:46:39 +0800

    add sold to reference to res.partner

--- wizard/real_estate_property_sold_to_wizard.py
+++ wizard/real_estate_property_sold_to_wizard.py
@@ -19,6 +19,8 @@ class RealEstatePropertySoldToWizard(models.TransientModel):
         active_id = self.env.context.get('active_id')
         property_record = self.env['estate.property'].browse(active_id)
         property_record.state = 'sold'
+        property_record.owner = self.create_property_owner().id
+
 
     @api.onchange('property_id')
     def _compute_selling_price(self):
@@ -30,3 +32,10 @@ class RealEstatePropertySoldToWizard(models.TransientModel):
     def _compute_agent_commission(self):
         for record in self:
             record.agent_commission = (record.selling_price * 6) / 100
+
+
+    def create_property_owner(self):
+        owner = self.env['property.owner'].search([('partner_id', '=', self.sold_to.id)], limit=1)
+        if not owner:
+            owner = self.env['property.owner'].create({'partner_id': self.sold_to.id})
+        return owner 
